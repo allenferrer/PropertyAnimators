50d30207cd7349e3222685cdad3d588a51188967
Allen Ferrer <allen.ferrer@xurpas.com>
2021-03-20 23:31:52 +0800

    Add state animation start Keys: 	 CGAffineTransform rotationAngle .pi 	CGAffineTransform.identity for view.transform

--- PropertyAnimators/ViewController.swift
+++ PropertyAnimators/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var block: UIView!
+    @IBOutlet weak var pauseButton: UIButton!
     
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var slider: UISlider!
     var animator: UIViewPropertyAnimator!
     
+    @IBOutlet weak var stopButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -22,15 +26,39 @@ class ViewController: UIViewController {
     }
 
     func setupAnimation() {
+        block.transform = CGAffineTransform.identity
+        block.frame.origin.x = CGFloat(0)
         animator = UIViewPropertyAnimator(duration: 1.2, curve: UIView.AnimationCurve.linear, animations: {
             let screenWidth = self.view.frame.size.width
             let blockWidth = self.block.frame.size.width
             self.block.frame.origin.x = screenWidth - blockWidth
+            self.block.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
         })
     }
     
     @IBAction func startAnimation(_ sender: Any) {
-        animator.startAnimation()
+        switch animator.state {
+        case .active:
+                break
+        case .inactive:
+            setupAnimation()
+            animator.startAnimation()
+            break
+        case .stopped:
+                break
+        default:
+            break
+        }
+
+    }
+    
+    @IBAction func pauseAnimation(_ sender: Any) {
+    }
+    
+    @IBAction func stopAnimation(_ sender: Any) {
+    }
+    
+    @IBAction func sliderDidChange(_ sender: Any) {
     }
 }
 
