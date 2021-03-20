5f2c2ea226b69e113be6ae6294509fa89f2cbba0
Allen Ferrer <allen.ferrer@xurpas.com>
2021-03-20 23:09:44 +0800

    Basic property animator implementation

--- PropertyAnimators/ViewController.swift
+++ PropertyAnimators/ViewController.swift
@@ -9,11 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var block: UIView!
+    
+    var animator: UIViewPropertyAnimator!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        setupAnimation()
+        
     }
 
-
+    func setupAnimation() {
+        animator = UIViewPropertyAnimator(duration: 1.2, curve: UIView.AnimationCurve.linear, animations: {
+            let screenWidth = self.view.frame.size.width
+            let blockWidth = self.block.frame.size.width
+            self.block.frame.origin.x = screenWidth - blockWidth
+        })
+    }
+    
+    @IBAction func startAnimation(_ sender: Any) {
+        animator.startAnimation()
+    }
 }
 
